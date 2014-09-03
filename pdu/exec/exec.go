2387656b80937dc510ece98d7ab9042f838d7b48
Tom Grennan <tom@apptimist.co>
2014-09-03 10:40:24 -0700

    changed exec to use \0 separated argument format
    Signed-off-by: Tom Grennan <tom@apptimist.co>

--- pdu/exec/exec.go
+++ pdu/exec/exec.go
@@ -6,7 +6,6 @@ package exec
 
 import (
 	"github.com/apptimistco/asn/pdu"
-	"github.com/tgrennan/quotation"
 	"strings"
 )
 
@@ -23,28 +22,9 @@ func NewExec(args ...string) *Exec {
 	return &e
 }
 
-func (p *Exec) bytes() []uint8 {
-	b := make([]uint8, 0, 4096)
-	for i, s := range *p {
-		if i > 0 {
-			b = append(b, ' ')
-		}
-		if strings.ContainsAny(s, " \t\n") {
-			b = append(b, '"')
-			b = append(b, s...)
-			b = append(b, '"')
-		} else {
-			b = append(b, s...)
-		}
-	}
-	return b
-}
-
 func (p *Exec) Format(version uint8, h pdu.Header) {
 	h.Write([]byte{version, pdu.ExecReqId.Version(version)})
-	b := p.bytes()
-	h.Write(b)
-	b = nil
+	h.Write([]byte(strings.Join(*p, "\x00")))
 }
 
 func (p *Exec) Id() pdu.Id { return pdu.ExecReqId }
@@ -55,10 +35,10 @@ func (p *Exec) Parse(h pdu.Header) pdu.Err {
 		return pdu.IlFormatErr
 	}
 	h.Next(2)
-	*p = quotation.Fields(string(h.Bytes()))
+	*p = strings.Split(string(h.Bytes()), "\x00")
 	return pdu.Success
 }
 
 func (p *Exec) String() string {
-	return string(p.bytes())
+	return strings.Join(*p, " ")
 }
