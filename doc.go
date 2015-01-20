33682a98e68ae1899efdba01cda3716872ef69c0
Tom Grennan <tom@apptimist.co>
2015-01-20 13:59:23 -0800

    Update copyright
    Signed-off-by: Tom Grennan <tom@apptimist.co>

--- doc.go
+++ doc.go
@@ -1,4 +1,4 @@
-// Copyright 2014 Apptimist, Inc. All rights reserved.
+// Copyright 2014-2015 Apptimist, Inc. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
